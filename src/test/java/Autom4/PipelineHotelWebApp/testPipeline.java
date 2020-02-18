e74a2551c0f24971e5d70e1adadbb64a2d8b6d11
YoannRose <roseyoann@gmail.com>
2020-02-18 15:15:58 +0100

    first commit

---
+++ src/test/java/Autom4/PipelineHotelWebApp/testPipeline.java
@@ -0,0 +1,45 @@
+package Autom4.PipelineHotelWebApp;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class testPipeline {
+	
+	
+	WebDriver driver = new FirefoxDriver();
+	String url= "http://192.168.102.133:9999/HotelWebapp/";
+	
+	
+	
+	@Before
+	public void setup() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+@Test
+public void testPipeline( ) {
+	driver.get(url);
+	WebElement recherche_ville = driver.findElement(By.xpath("//select[@name=\"ville\"]"));
+	recherche_ville.click();
+	WebElement ville_lumiere = driver.findElement(By.xpath("//option[.='Paris']"));
+	ville_lumiere.click();
+	
+	WebElement button_submit = driver.findElement(By.xpath("//button"));
+	button_submit.click();
+	
+	WebElement tableau = driver.findElement(By.xpath("//tbody"));
+	System.out.println(tableau.getText());
+}
+	
+	
+	
+	
+	
+}
